da1275cf92bec8a6f034e76acce3a53fb38d4afe
matan793 <mataneldar123@gamail.com>
2025-06-04 14:51:00 +0300

    changed schemma

--- SQL/ecommerce.sql
+++ SQL/ecommerce.sql
@@ -1,7 +1,33 @@
 DROP SCHEMA IF EXISTS ecommerce;
 DROP TABLE IF EXISTS ecommerce.users;
+DROP TABLE IF EXISTS ecommerce.addresses;
+DROP TABLE IF EXISTS ecommerce.categories;
+DROP TABLE IF EXISTS ecommerce.products;
+DROP TABLE IF EXISTS ecommerce.orders;
+DROP TABLE IF EXISTS ecommerce.order_items;
+DROP TABLE IF EXISTS ecommerce.payments;
+DROP TABLE IF EXISTS ecommerce.cart;
+DROP TYPE IF EXISTS ecommerce.order_status;
 
 CREATE SCHEMA ecommerce;
+
+CREATE TYPE ecommerce.order_status AS ENUM(
+'pending',
+'processing',
+'cancelled',
+'deliverd'
+);
+
+CREATE TYPE ecommerce.payment_status(
+'pending',
+'cancelled',
+'approved'
+);
+
+CREATE TYPE ecommerce.payment_method(
+'credit',
+'paypal'
+)
 
 CREATE TABLE ecommerce.users(
 	userId SERIAL PRIMARY KEY,
@@ -43,14 +69,14 @@ CREATE TABLE ecommerce.orders (
     orderId SERIAL PRIMARY KEY,
     userId INTEGER REFERENCES ecommerce.users(userId) ON DELETE SET NULL,
     addressId INTEGER REFERENCES addresses(addressId),
-    status TEXT DEFAULT 'pending', -- todo::: a enum
+    status ecommerce.ORDER_STATUS  DEFAULT 'pending', -- todo::: a enum
     total_amount NUMERIC(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE ecommerce.order_items (
     id SERIAL PRIMARY KEY,
-    orderId INTEGER REFERENCES orders(orderId) ON DELETE CASCADE,
+    orderId INTEGER REFERENCES ecommerce.orders(orderId) ON DELETE CASCADE,
     productId INTEGER REFERENCES ecommerce.products(productId),
     quantity INTEGER NOT NULL,
     unit_price NUMERIC(10, 2) NOT NULL -- snapshot of price at time of purchase
@@ -59,10 +85,14 @@ CREATE TABLE ecommerce.order_items (
 CREATE TABLE ecommerce.payments (
     paymentId SERIAL PRIMARY KEY,
     orderId INTEGER REFERENCES ecommerce.orders(orderId) ON DELETE CASCADE,
-    payment_method TEXT NOT NULL, -- todo::: enum
-    payment_status TEXT DEFAULT 'pending', -- enum
+    payment_method ecommerce.PAYMENT_METHOD NOT NULL, -- todo::: enum
+    payment_status ecommerce.PAYMENT_STATUS DEFAULT 'pending', -- enum
     amount NUMERIC(10, 2) NOT NULL,
     paid_at TIMESTAMP
 );
 
--- todo::: cart
+CREATE TABLE eommerce.cart(
+userId INTEGER REFERENCES ecommerce.users(userId) ON DELETE CASCADE,
+productId INTEGER REFERENCES ecommerce.products(productId) ON DELETE CASCADE,
+CONSTRAINT PK_Cart PRIMARY KEY (userID,productId)
+)
